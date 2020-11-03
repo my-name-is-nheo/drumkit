1e4bb11bbce7b682af6ffea8a1fe2a91e7199a40
Nicholas Heo <nicholas.heo95@gmail.com>
2020-11-03 17:15:20 -0500

    progress

--- client/src/components/Drumkit/Drumkit.jsx
+++ client/src/components/Drumkit/Drumkit.jsx
@@ -5,10 +5,28 @@ export default class DrumKit extends Component {
     super(props);
     this.state = {};
   }
+  handleKeyPress = (e) => {
+    console.log(e);
+  };
   render() {
     return (
       <div id="drumkit">
-        <img class="drums" draggable="false" src={"/images/drumkit.png"} />
+        <img className="drums" draggable="false" src={"/images/drumkit.png"} />
+        <button id="bass" disabled="true">
+          J
+        </button>
+        <button id="snare" disabled="true">
+          F
+        </button>
+        <button id="low-tom" disabled="true">
+          ;
+        </button>
+        <button id="med-tom" disabled="true">
+          L
+        </button>
+        <button id="high-tom" disabled="true">
+          K
+        </button>
       </div>
     );
   }
